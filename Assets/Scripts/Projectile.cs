cc3aad672e99a8a3c9ab6d8a1193e4250280f40b
Владимир Клешнин <kleshnin99@gmail.com>
2023-11-17 09:12:06 +0300

    fix: Unnecessary field removed.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class Projectile : MonoBehaviour
 {
-    /// <summary>RigidBody2D снаряда.</summary>
-    public Rigidbody2D Rigidbody { get; private set; }
-    
-
-    private void Start()
-    {
-        Rigidbody = GetComponent<Rigidbody2D>();
-    }
-
     private void OnBecameInvisible()
     {
         Destroy(gameObject, 2f);
